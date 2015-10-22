5806ed111e398ef5ae6c891dca85aeb73beab38b
Ben Burton <benjamin.joseph.burton@gmail.com>
2015-10-22 14:26:23 -0400

    Wire up a 'progresstesting' converter

--- src/main/scala/com/progresstesting/qti/ItemExtractor.scala
+++ src/main/scala/com/progresstesting/qti/ItemExtractor.scala
@@ -1,16 +1,16 @@
-package com.keydatasys.conversion.qti
+package com.progresstesting.qti
 
+import com.keydatasys.conversion.qti.ItemTransformer
 import com.keydatasys.conversion.qti.manifest.ManifestReader
-import com.keydatasys.conversion.qti.util.{PathFlattener, HtmlProcessor, PassageTransformer}
 import org.corespring.common.file.SourceWrapper
 import org.corespring.conversion.qti.AbstractItemExtractor
 import org.corespring.conversion.qti.manifest.QTIManifest
-import play.api.libs.json._
+import play.api.libs.json.{Json, JsValue, JsObject}
 
 import scalaz.{Failure, Success, Validation}
 
 class ItemExtractor(sources: Map[String, SourceWrapper], commonMetadata: JsObject)
-  extends AbstractItemExtractor with PassageTransformer with HtmlProcessor with PathFlattener {
+  extends AbstractItemExtractor {
 
   val manifest: Option[QTIManifest] = sources.find{ case(filename, _) => filename == ManifestReader.filename }
     .map { case(_, manifest) => ManifestReader.read(manifest, sources) }
@@ -23,12 +23,12 @@ class ItemExtractor(sources: Map[String, SourceWrapper], commonMetadata: JsObjec
     )).getOrElse(Seq.empty).toMap
 
   def filesFromManifest(id: String) = manifest.map(m => m.items.find(_.id == id)).flatten.map(item => item.resources)
-    .getOrElse(Seq.empty).map(_.path.flattenPath)
+    .getOrElse(Seq.empty).map(_.path)
 
   lazy val itemJson: Map[String, Validation[Error, JsValue]] =
-    manifest.map(_.items.map(f => sources.get(f.filename.flattenPath).map(s => {
+    manifest.map(_.items.map(f => sources.get(f.filename).map(s => {
       try {
-        f.id -> Success(ItemTransformer.transform(preprocessHtml(s.getLines.mkString), f, sources))
+        f.id -> Success(ItemTransformer.transform(s.getLines.mkString, f, sources))
       } catch {
         case e: Exception => {
           println(e.getMessage)
---
+++ src/main/scala/com/progresstesting/zip/ProgressTestingQtiZipConverter.scala
@@ -0,0 +1,95 @@
+package com.progresstesting.zip
+
+import java.io.{FileOutputStream, File, ByteArrayOutputStream}
+import java.util.zip.{ZipEntry, ZipOutputStream, ZipFile}
+
+import com.keydatasys.conversion.qti.ItemExtractor
+import com.keydatasys.conversion.zip.KDSQtiZipConverter._
+import org.corespring.common.file.SourceWrapper
+import org.corespring.conversion.zip.QtiToCorespringConverter
+import play.api.libs.json.{JsValue, Json, JsObject}
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+import scalaz.{Success, Failure, Validation}
+
+/**
+ * Created by bburton on 10/22/15.
+ */
+class ProgressTestingQtiZipConverter extends QtiToCorespringConverter {
+
+  private val collectionName = "progresstesting"
+  private val collectionId = "5453b4e4e4b05f38dd6440a8"
+
+  override def convert(zip: ZipFile, path: String = "target/corespring-json.zip", metadata: Option[JsObject] = None): ZipFile = {
+
+    val fileMap = zip.entries.filterNot(_.isDirectory).map(entry => {
+      entry.getName -> SourceWrapper(entry.getName, zip.getInputStream(entry))
+    }).toMap
+
+    val extractor = new ItemExtractor(fileMap, metadata.getOrElse(Json.obj()))
+    val itemCount = extractor.ids.length
+    val processedFiles = extractor.ids.zipWithIndex.map{ case(id, index) => {
+      println(s"Processing ${id} (${index+1}/$itemCount)")
+      val itemJson = extractor.itemJson
+      val meta = extractor.metadata
+      val result: Validation[Error, (JsValue, JsObject
+        )] = (itemJson.get(id).getOrElse(Failure(new Error("Missing item JSON"))),
+        meta.get(id).getOrElse(Failure(new Error("Missing item metadata")))) match {
+        case (Failure(error), _) => Failure(error)
+        case (_, Failure(error)) => Failure(error)
+        case (Success(itemJson), Success(md)) => {
+          implicit val metadata = md
+          Success((itemJson, taskInfo))
+        }
+      }
+      result match {
+        case Success((json, taskInfo)) => {
+          val basePath = s"${collectionName}_${collectionId}/$id"
+          Seq(s"$basePath/player-definition.json" -> Source.fromString(Json.prettyPrint(json)),
+            s"$basePath/profile.json" -> Source.fromString(Json.prettyPrint(
+              Json.obj("taskInfo" -> taskInfo, "originId" -> id)))) ++
+            extractor.filesFromManifest(id).map(filename => s"$basePath/data/${filename.flattenPath}" -> fileMap.get(filename))
+              .filter { case (filename, maybeSource) => maybeSource.nonEmpty }
+              .map { case (filename, someSource) => (filename, someSource.get.toSource()) }
+        }
+        case _ => Seq.empty[(String, Source)]
+      }
+    }}.flatten.toMap
+    writeZip(toZipByteArray(processedFiles), path)
+  }
+
+  private def taskInfo(implicit metadata: Option[JsValue]): JsObject = {
+    partialObj(
+      "relatedSubject" -> Some(Json.arr()),
+      "domains" -> Some(Json.arr()),
+      "extended" -> metadata.map(md => Json.obj(
+        "kds" -> md
+      ))
+    )
+  }
+
+  private def writeZip(byteArray: Array[Byte], path: String) = {
+    val file = new File(path)
+    val fileOutput = new FileOutputStream(file)
+    try {
+      fileOutput.write(byteArray)
+    } finally {
+      fileOutput.close()
+    }
+    new ZipFile(file)
+  }
+
+  private def toZipByteArray(files: Map[String, Source]) = {
+    val bos = new ByteArrayOutputStream()
+    val zipFile = new ZipOutputStream(bos)
+    files.foreach{ case (filename, contents) => {
+      zipFile.putNextEntry(new ZipEntry(filename))
+      zipFile.write(contents.map(_.toByte).toArray)
+    }}
+    zipFile.close
+    bos.toByteArray
+  }
+
+
+}
