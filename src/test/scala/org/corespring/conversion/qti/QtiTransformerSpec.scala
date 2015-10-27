0b3ab9cb1a2d3144d293e8a4c3dd9647c279973a
Ben Burton <benjamin.joseph.burton@gmail.com>
2015-10-27 11:55:02 -0400

    Scope input data to clarify setup
    This adds a new "should" block for 
    textEntryInteraction to clarify the context of the
    input data so that its setup is distinct from 
    other, similar specs.

--- src/test/scala/org/corespring/conversion/qti/QtiTransformerSpec.scala
+++ src/test/scala/org/corespring/conversion/qti/QtiTransformerSpec.scala
@@ -7,25 +7,31 @@ import scala.xml.XML
 
 class QtiTransformerSpec extends Specification {
 
-  val qti =
-    <assessmentItem>
-      <responseDeclaration identifier="Q_01" cardinality="single" baseType="string">
-        <correctResponse>
-          <value>397.66</value>
-          <value>397.7</value>
-        </correctResponse>
-      </responseDeclaration>
-      <itemBody>
-        <textEntryInteraction responseIdentifier="Q_01" expectedLength="10"/>
-      </itemBody>
-    </assessmentItem>
-
-
  "transform" should {
-   "transform bad qti found on qa" in {
-     val json = QtiTransformer.transform(qti)
-     (json \ "components").asOpt[JsObject].isDefined === true
+
+   "with a textEntryInteraction" should {
+
+     val qti =
+       <assessmentItem>
+         <responseDeclaration identifier="Q_01" cardinality="single" baseType="string">
+           <correctResponse>
+             <value>397.66</value>
+             <value>397.7</value>
+           </correctResponse>
+         </responseDeclaration>
+         <itemBody>
+           <textEntryInteraction responseIdentifier="Q_01" expectedLength="10"/>
+         </itemBody>
+       </assessmentItem>
+
+     "transform bad qti found on qa" in {
+       val json = QtiTransformer.transform(qti)
+       (json \ "components").asOpt[JsObject].isDefined === true
+     }
+
    }
+
+
  }
 
 }
