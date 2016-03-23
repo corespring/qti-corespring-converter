3e8b74c5f33596890c92ffe745bca3127681c5b4
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-03-24 07:57:00 +1300

    Adjust GGM correct answer

--- src/main/scala/org/parcconline/conversion/qti/interactions/GraphicGapMatchInteractionTransformer.scala
+++ src/main/scala/org/parcconline/conversion/qti/interactions/GraphicGapMatchInteractionTransformer.scala
@@ -34,13 +34,27 @@ class GraphicGapMatchInteractionTransformer(sources: Map[String, SourceWrapper])
     }
   }
 
+  private def correctResponses(id: String, qti: Node) =
+    (((qti \\ "responseDeclaration").find(n => (n \ "@identifier").text == id) match {
+      case Some(node) => node
+      case _ => throw new IllegalArgumentException(s"QTI does not contain responseDeclaration for $id")
+    }) \\ "value").toSeq.map(n => JsString(n.text.trim)).map { cr =>
+      val idHotspotRegex = """([^\s]*) ([^\s]*)""".r
+      cr.asOpt[String] match {
+        case Some(idHotspotRegex(id, hotspot)) => Json.obj(
+          "id" -> id,
+          "hotspot" -> hotspot)
+        case _ => Json.obj()
+      }
+    }
+
   override def interactionJs(qti: Node, manifest: Node) = super.interactionJs(qti, manifest).map{ case(id, json) => {
     val imageFilename = (json \ "model" \ "config" \ "backgroundImage" \ "path").as[String]
     val (width, height) = sources.find{ case(filename, _) => filename.endsWith(imageFilename) } match {
       case Some((_, file)) => SVGZConverter.getDimensions(file)
       case _ => throw new Exception(s"Could not find image $imageFilename")
     }
-    id -> json.deepMerge(Json.obj(
+    id -> (json.deepMerge(Json.obj(
       "feedback" -> Json.obj(
         "correctFeedbackType" -> "none",
         "partialFeedbackType" -> "none",
@@ -57,7 +71,7 @@ class GraphicGapMatchInteractionTransformer(sources: Map[String, SourceWrapper])
         ),
         "choices" -> choices(json)
       )
-    ))
+    )).as[JsObject] ++ Json.obj("correctResponse" -> correctResponses(id, qti)))
   }}
 
   implicit class WithoutDimensions(elem: Elem) {
