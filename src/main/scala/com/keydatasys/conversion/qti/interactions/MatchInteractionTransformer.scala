64213520e7fef5babbf4dc502585e7aa704aaeb0
benburton <benjamin.joseph.burton@gmail.com>
2017-04-05 11:25:32 -0400

    More lenient columns

--- src/main/scala/com/keydatasys/conversion/qti/interactions/MatchInteractionTransformer.scala
+++ src/main/scala/com/keydatasys/conversion/qti/interactions/MatchInteractionTransformer.scala
@@ -128,6 +128,6 @@ object MatchInteractionTransformer extends InteractionTransformer {
     ))
   }
 
-  private val numberStrings = Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five", 6 -> "six")
+  private val numberStrings = Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five", 6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine", 10 -> "ten")
 
 }
