db351c25e38dcc54942c96da95c3b27754f661f7
edeustace <ed.eustace@gmail.com>
2017-11-07 21:51:28 -0500

    add some notes about the < conundrum

--- src/main/scala/org/corespring/conversion/qti/manifest/ManifestItem.scala
+++ src/main/scala/org/corespring/conversion/qti/manifest/ManifestItem.scala
@@ -43,6 +43,23 @@ object ZipReader extends PassageScrubber with EntityEscaper {
 
   lazy val logger = LoggerFactory.getLogger(ZipReader.this.getClass)
 
+  /**
+    * TODO:
+    * We have an issues where by stripping the cdata tags we run the risk of having invalid xml eg:
+    * <node><![CDATA[a<b]]></node> -> <node>a<b</node> <--- this is invalid xml
+    * However this step is needed because there qti sometimes contains xml within the cdata that we need to parse and transform:
+    * <node><![CDATA[<audio>..</audio>]]></node>
+    *
+    * We can't just escape < and > because doing so will turn <audio> to &lt;audio&gt; which won't get parsed.
+    *
+    * The best option for now is try and 1. jsoup it, 2. then parse it - it it parses then don't escape <,> - if it doesnt parse, then escape them???
+    *
+    * OR:
+    * look for <audio ... or <video .. in the string and if present then dont escape ?
+    *
+    * @param xmlString
+    * @return
+    */
   private def stripCDataTags(xmlString: String) =
     """(?s)<!\[CDATA\[(.*?)\]\]>""".r.replaceAllIn(xmlString, "$1")
 
