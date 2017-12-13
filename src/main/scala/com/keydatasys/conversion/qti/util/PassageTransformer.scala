1dfedcc401d7b6455c507487c4acde38b9f23e57
edeustace <ed.eustace@gmail.com>
2017-12-13 11:52:25 -0500

    handle passage parse failures

--- src/main/scala/com/keydatasys/conversion/qti/util/PassageTransformer.scala
+++ src/main/scala/com/keydatasys/conversion/qti/util/PassageTransformer.scala
@@ -2,17 +2,27 @@ package com.keydatasys.conversion.qti.util
 
 import org.corespring.common.file.SourceWrapper
 import org.corespring.common.util.HtmlProcessor
-import org.corespring.conversion.qti.manifest.{ManifestResourceType, ManifestResource}
+import org.corespring.conversion.qti.manifest.{ManifestResource, ManifestResourceType}
+import org.slf4j.LoggerFactory
 
 import scala.xml.XML
 
 trait PassageTransformer extends PassageScrubber with HtmlProcessor with PathFlattener {
 
+  val ptLogger = LoggerFactory.getLogger(PathTransformer.getClass)
+
   def transformPassage(resource: ManifestResource)(implicit sources: Map[String, SourceWrapper]): Option[String] = {
     resource.resourceType == ManifestResourceType.Passage match {
       case true => {
         sources.find{ case (path, source) => resource.path.flattenPath == path.flattenPath }.map(_._2) match {
-          case Some(source) => Some(transformPassage(source.getLines.mkString))
+          case Some(source) => try {
+            Some(transformPassage(source.getLines.mkString))
+          } catch {
+            case t : Throwable => {
+              ptLogger.error(s"Error reading passage ${resource.path}: ${t.getMessage}")
+              None
+            }
+          }
           case _ => {
             println(s"Missing passage ${resource.path}")
             None
