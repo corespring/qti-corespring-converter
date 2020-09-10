36fe1be6012f27a2c8971395a2cc2406e0c29cca
ed eustace <ed.eustace@gmail.com>
2020-09-10 16:43:54 +0100

    feat: Update App to allow file in/out + mode

--- src/main/scala/org/corespring/conversion/qti/Runner.scala
+++ src/main/scala/org/corespring/conversion/qti/Runner.scala
@@ -17,114 +17,53 @@ import scala.io.Source
 case class RunOpts(
                     input: String,
                     output: String,
-                    vendor: String,
-                    metadata: String = "{}",
-                    limit: Int = 0,
-                    sourceIds: Seq[String] = Seq.empty,
-                    killRuntime: Boolean = true
+                    mode: String
                   )
 
 
-object Runner extends App {
+object Run extends App {
+
+    val version = {
+
+    }
+
+    println(qtiConverter.BuildInfo.toString)
+
+    val logger = LoggerFactory.getLogger(Run.this.getClass)
+
+    val parser = new scopt.OptionParser[RunOpts]("run") {
+      head("run", "version")
 
 
-  val version = {
+      opt[String]('i', "input").required().action((i, c) => c.copy(input = i))
 
-  }
+      opt[String]('o', "output").required().action((o, c) => c.copy(output = o))
 
-  println(qtiConverter.BuildInfo.toString)
+      opt[String]('m', "mode").required().action( (o,c) => c.copy(mode = o))
 
-  val logger = LoggerFactory.getLogger(Runner.this.getClass)
 
-  val parser = new scopt.OptionParser[RunOpts]("run") {
-    head("run", "version")
+    }
 
-    opt[Int]('l', "limit").action((l, c) => c.copy(limit = l))
 
-    opt[String]('i', "input").required().action((i, c) => c.copy(input = i))
 
-    opt[String]('o', "output").required().action((o, c) => c.copy(output = o))
+    parser.parse(args, RunOpts("", "", "")) match {
+      case Some(runOpts) => {
+        logger.info(s"${runOpts}")
 
-    opt[String]('v', "vendor")
-      .required().action((v, c) => c.copy(vendor = v))
-      .validate(v => {
-        val vendors = converters.keys.toSeq
-        if (vendors.contains(v)) success else failure("unknown vendor")
-      })
-
-    opt[String]('m', "metadata")
-      .action((m, c) => c.copy(metadata = m))
-      .validate(m => {
-        try {
-          Json.parse(m).as[JsObject]
-          success
-        } catch {
-          case t: Throwable => failure("not valid json")
+        if(runOpts.mode == "single-file"){
+          // convert a single qti file here ....
+          logger.info("convert the file to pie json here ... this is just to get things started .. ")
+        } else {
+          logger.info("TODO...")
         }
-      })
-
-    opt[String]('s', "sourceId").action((s, c) => c.copy(sourceIds = c.sourceIds ++ Seq(s)))
-
-    opt[String]('l', "sourceIdList")
-      .text("A path to a file with a source id on its own line")
-      .action((l, c) => {
-        val ids = Source.fromFile(new File(l))
-          .getLines()
-          .map(_.trim)
-          .filterNot(_.isEmpty)
-          .toSeq
-          .distinct
-        c.copy(sourceIds = c.sourceIds ++ ids)
-      })
-
-    opt[Boolean]('r', "killRuntime").action((r, c) => c.copy(killRuntime = r))
-  }
-
-
-  val converters = Map(
-    "kds" -> KDSQtiZipConverter,
-    "measuredprogress" -> MeasuredProgressQtiZipConverter,
-  )
-
-  parser.parse(args, RunOpts("", "", "")) match {
-    case Some(runOpts) => {
-      val input = new ZipFile(runOpts.input)
-      val outputPath = runOpts.output
-      val vendor = runOpts.vendor
-      val metadata = Json.parse(runOpts.metadata).as[JsObject]
-
-      val converter = converters
-        .get(vendor).getOrElse(throw new IllegalArgumentException(s"You must specify a supported vendor: ${converters.keys.mkString(", ")}"))
-
-      val opts = ConversionOpts(
-        runOpts.limit,
-        runOpts.sourceIds
-      )
-
-
-      logger.info(s"convertion opts: $opts")
-
-      val outFile = new File(runOpts.output)
-
-      if (outFile.exists()) {
-        logger.info(s"Deleting ${runOpts.output}")
-        outFile.delete
       }
-
-      result(converter.convert(input, outputPath, Some(metadata), opts)
-        .map(_ => {
-          println(s"all done sourceIds: ${opts.sourceIds}")
-
-          if (runOpts.killRuntime) {
-            Runtime.getRuntime().halt(0)
-          }
-        }), 25.minutes)
+      case None => {
+        println(parser.usage)
+        //sys.exit(1)
+      }
     }
-    case None => {
-      println(parser.usage)
-      sys.exit(1)
-    }
-  }
+
+
 
 }
 
